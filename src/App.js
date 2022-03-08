df2a58f0fadf283648492ca9214616c3f2ab4c81
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-08 12:45:21 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+
+  const hello = "hey there";
+
+  const printStuff = (message) => {
+    console.log(message);
+  }
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                
-            </header>
-          <h1>HI THERE</h1>
-        </div>
+        <>
+            <div className="App">
+                <h1>1+2</h1>
+                <h1>{1+2}</h1>
+                <h2><marquee>hello</marquee></h2>
+                <h2><marquee>{hello}</marquee></h2>
+
+                <button onClick={printStuff}>click me!!!</button>
+                <button onClick={() => {printStuff("Hi from second button")}}>New Click</button>
+            </div>
+        </>
     );
 }
 
